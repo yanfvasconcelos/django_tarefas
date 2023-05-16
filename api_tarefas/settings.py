bafae4ced045e9d9cf41653f01db11b3d6fce1ea
yanfvasconcelos <yanfelipe1515@gmail.com>
2023-05-16 15:03:35 -0300

    API Tarefas

--- api_tarefas/settings.py
+++ api_tarefas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)4*!3h5-@0emxt#e14#le(!$*)=03-zdtk#g1ur5%(bf^euj18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(config('DEBUG'))
 
-ALLOWED_HOSTS = ['https://django-tarefas.onrender.com', 'https//:localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-tarefas.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
